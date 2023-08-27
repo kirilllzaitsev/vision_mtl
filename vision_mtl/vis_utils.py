b8eedfae1470e34e208197a8a327f006ef5560ff
kirilllzaitsev <kzaitsev88@gmail.com>
2023-08-27 23:00:29 +0200

    change plot_annotated_segm_mask to use legend

--- vision_mtl/vis_utils.py
+++ vision_mtl/vis_utils.py
@@ -1,5 +1,9 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.patches import Rectangle
+
+from vision_mtl.cfg import cfg
 
 
 def plot_segm_class(idx, mask):
@@ -7,6 +11,30 @@ def plot_segm_class(idx, mask):
     empty_canvas[mask == idx, ...] = 255
     print(empty_canvas.max(), empty_canvas.min())
     plt.imshow(empty_canvas)
+
+
+def plot_annotated_segm_mask(mask, class_names, img=None, alpha=1.0):
+    def remap_class_idx_to_rgb(mask, palette):
+        rgb_mask = np.zeros((*mask.shape, 3), dtype=np.uint8)
+        for idx, class_idx in enumerate(np.unique(mask)):
+            rgb_mask[mask == class_idx, ...] = palette[class_idx]
+        return rgb_mask
+
+    colored_rgb_palette = cfg.vis.rgb_palette
+
+    legend_data = [(colored_rgb_palette[k], class_names[k]) for k in np.unique(mask)]
+
+    handles = [
+        Rectangle((0, 0), 1, 1, color=[v / 255 for v in c]) for c, _ in legend_data
+    ]
+    labels = [n for _, n in legend_data]
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    if img is not None:
+        ax.imshow(img)
+    plt.imshow(remap_class_idx_to_rgb(mask, colored_rgb_palette), alpha=alpha)
+    plt.legend(handles, labels)
+    plt.show()
 
 
 def plot_annotated_segm_mask_v1(mask, class_names):
