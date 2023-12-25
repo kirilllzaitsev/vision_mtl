a834d66f202fe1916e693917f6826933c63834fb
kirilllzaitsev <kzaitsev88@gmail.com>
2023-12-25 14:55:57 +0100

    rm ->uint transform

--- vision_mtl/data/cityscapes.py
+++ vision_mtl/data/cityscapes.py
@@ -28,33 +28,28 @@ class PhotopicVisionDataset(Dataset):
     def __len__(self) -> int:
         return len(self.paths)
 
-    def __getitem__(self, idx) -> tuple:
+    def __getitem__(self, idx) -> dict:
         data_path, mask_path, depth_path = (
             self.paths["img"][idx],
             self.paths["mask"][idx],
             self.paths["depth"][idx],
         )
         img = np.load(data_path)
-        assert img.max() <= 1.0
         mask = np.load(mask_path)
-        depth = np.load(depth_path).squeeze()
+        depth = np.load(depth_path)
         if self.transforms:
-            img *= 255
-            img = img.astype(np.uint8)
             transformed = self.transforms(image=img, mask=mask)
-            transformed_depth = self.transforms(image=img, mask=depth)
-            img, mask, depth = (
                 transformed["image"],
                 transformed["mask"],
                 transformed_depth["mask"],
             )
-            img = img.float() / 255
 
             mask = mask.long()
         else:
             mask = torch.from_numpy(mask).long()
             depth = torch.from_numpy(depth).float()
 
+            img = torch.from_numpy(img).float()
         sample = {"img": img, "mask": mask, "depth": depth}
         return sample
 
