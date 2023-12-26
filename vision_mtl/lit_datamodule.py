aa18a17c199bb1f2b46d3a5a88f01a141f27c9b1
kirilllzaitsev <kzaitsev88@gmail.com>
2023-12-26 11:12:41 +0100

    add do_overfit to ds gen

--- vision_mtl/lit_datamodule.py
+++ vision_mtl/lit_datamodule.py
@@ -7,11 +7,12 @@ import torch
 import torchvision.transforms as T
 from torch.utils.data import DataLoader
 
-from vision_mtl.data.cityscapes import PhotopicVisionDataset
+from vision_mtl.data.ds_cityscapes import CityscapesDataset
 from vision_mtl.data.transforms import test_transform, train_transform
 
 
 class PhotopicVisionDataModule(pl.LightningDataModule):
+
     def __init__(
         self,
         data_base_dir: str,
@@ -20,50 +21,64 @@ class PhotopicVisionDataModule(pl.LightningDataModule):
         train_size: float = 0.8,
         batch_size: int = 4,
         num_workers: int = 4,
+        do_overfit: bool = False,
     ):
         super().__init__()
         self.data_base_dir = data_base_dir
+        self.do_overfit = do_overfit
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.train_size = train_size
+        self.data_train: CityscapesDataset = None
+        self.data_val: CityscapesDataset = None
+        self.data_test: CityscapesDataset = None
+        self.data_predict: CityscapesDataset = None
         self.train_transform = train_transform
         self.test_transform = test_transform
-        self.data_train = None
-        self.data_val = None
-        self.data_test = None
-        self.data_predict = None
         self.save_hyperparameters()
 
     def setup(self, stage: str = None) -> None:
+        data_train = CityscapesDataset(
+            stage="train",
+            data_base_dir=self.data_base_dir,
+            transforms=self.train_transform,
+        )
         if stage == "fit" or stage is None:
-            data_train = PhotopicVisionDataset(
-                stage="train",
-                data_base_dir=self.data_base_dir,
-                transforms=self.train_transform,
-            )
 
-            train_len = int(len(data_train) * self.train_size)
+            if self.do_overfit:
+                self.data_val = self.data_train = torch.utils.data.Subset(
+                    data_train,
+                    range(0, self.batch_size),
+                )
+            else:
+                train_len = int(len(data_train) * self.train_size)
 
-            self.data_train, self.data_val = torch.utils.data.random_split(
-                data_train,
-                [
-                    train_len,
-                    len(data_train) - train_len,
-                ],
-            )
-            self.data_val.transforms = self.test_transform
+                self.data_train, self.data_val = torch.utils.data.random_split(
+                    data_train,
+                    [
+                        train_len,
+                        len(data_train) - train_len,
+                    ],
+                )
+                self.data_val.transforms = self.test_transform
         if stage == "test" or stage is None:
-            self.data_test = PhotopicVisionDataset(
-                stage="val",
-                data_base_dir=self.data_base_dir,
-                transforms=self.test_transform,
-            )
+            if self.do_overfit:
+                self.data_test = self.data_train
+            else:
+                self.data_test = CityscapesDataset(
+                    stage="val",
+                    data_base_dir=self.data_base_dir,
+                    transforms=self.test_transform,
+                )
         if stage == "predict" or stage is None:
-            self.data_predict = PhotopicVisionDataset(
-                stage="val",
-                data_base_dir=self.data_base_dir,
-                transforms=self.test_transform,
-            )
+            if self.do_overfit:
+                self.data_predict = self.data_train
+            else:
+                self.data_predict = CityscapesDataset(
+                    stage="val",
+                    data_base_dir=self.data_base_dir,
+                    transforms=self.test_transform,
+                )
 
     def teardown(self, stage: str) -> None:
         return super().teardown(stage)
@@ -73,7 +88,7 @@ class PhotopicVisionDataModule(pl.LightningDataModule):
             dataset=self.data_train,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
-            shuffle=True,
+            shuffle=False,
         )
 
     def val_dataloader(self) -> DataLoader:
