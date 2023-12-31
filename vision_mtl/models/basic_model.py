09db3a1d2badbbd13a2983cc9e8ce3edac7ac813
kirilllzaitsev <kzaitsev88@gmail.com>
2023-12-31 18:08:13 +0100

    refactor model into backbone and heads

--- vision_mtl/models/basic_model.py
+++ vision_mtl/models/basic_model.py
@@ -4,6 +4,42 @@ import torch.nn as nn
 from segmentation_models_pytorch.base import SegmentationHead
 
 
+class Backbone(nn.Module):
+    def __init__(
+        self,
+        encoder_name="timm-mobilenetv3_large_100",
+        encoder_weights="imagenet",
+        decoder_first_channel=256,
+        num_decoder_layers=5,
+        in_channels=3,
+    ):
+        super().__init__()
+
+        self.decoder_channels = [decoder_first_channel]
+        for i in range(1, num_decoder_layers):
+            self.decoder_channels.append(decoder_first_channel // (2**i))
+
+        model = smp.Unet(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            in_channels=in_channels,
+            encoder_depth=len(self.decoder_channels),
+            decoder_channels=self.decoder_channels,
+        )
+
+        self.encoder = model.encoder
+        self.decoder = model.decoder
+
+    def forward(self, x):
+        """Sequentially pass `x` trough model`s encoder, decoder and heads"""
+
+        features = self.encoder(x)
+
+        decoder_output = self.decoder(*features)
+
+        return decoder_output
+
+
 class BasicMTLModel(nn.Module):
     def __init__(
         self,
@@ -17,25 +53,21 @@ class BasicMTLModel(nn.Module):
     ):
         super().__init__()
 
-        decoder_channels = [decoder_first_channel]
-        for i in range(1, num_decoder_layers):
-            decoder_channels.append(decoder_first_channel // (2**i))
-
-        model = smp.Unet(
+        self.backbone = Backbone(
             encoder_name=encoder_name,
             encoder_weights=encoder_weights,
+            decoder_first_channel=decoder_first_channel,
+            num_decoder_layers=num_decoder_layers,
             in_channels=in_channels,
-            classes=segm_classes,
-            activation=activation,
-            encoder_depth=len(decoder_channels),
-            decoder_channels=decoder_channels,
         )
-
-        self.encoder = model.encoder
-        self.decoder = model.decoder
-        self.segm_head = model.segmentation_head
+        self.segm_head = SegmentationHead(
+            in_channels=self.backbone.decoder_channels[-1],
+            out_channels=segm_classes,
+            activation=activation,
+            kernel_size=3,
+        )
         self.depth_head = SegmentationHead(
-            in_channels=decoder_channels[-1],
+            in_channels=self.backbone.decoder_channels[-1],
             out_channels=1,
             activation=activation,
             kernel_size=3,
@@ -44,9 +76,7 @@ class BasicMTLModel(nn.Module):
     def forward(self, x):
         """Sequentially pass `x` trough model`s encoder, decoder and heads"""
 
-        features = self.encoder(x)
-
-        decoder_output = self.decoder(*features)
+        decoder_output = self.backbone(x)
 
         depth = self.depth_head(decoder_output)
         segm = self.segm_head(decoder_output)
@@ -67,4 +97,4 @@ if __name__ == "__main__":
     model = BasicMTLModel()
     x = torch.randn(1, 3, 256, 256)
     y = model(x)
-    print(y.shape)
+    print(y["depth"].shape)
