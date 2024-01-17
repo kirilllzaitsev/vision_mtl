7bbcb907c2fd5011bd27f97b95fd112c70a1100e
kirilllzaitsev <kzaitsev88@gmail.com>
2024-01-17 22:01:07 +0100

    black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='vision_mtl',
+    name="vision_mtl",
     packages=find_packages(),
-    version='0.1.0',
-    description='Multi-task learning pipeline for vision tasks',
-    author='Kirill Zaitsev',
-    license='',
+    version="0.1.0",
+    description="Multi-task learning pipeline for vision tasks",
+    author="Kirill Zaitsev",
+    license="",
 )
